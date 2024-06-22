b18e63c3e6bc7c07f2f4c1918644009cbbb6a723
Didier <did35@yahoo.com>
2024-06-22 08:51:58 +0200

    Initial Commit

---
+++ TimeLapse/TimeLapseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLapseApp.swift
+//  TimeLapse
+//
+//  Created by Didier Delhaisse on 22/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimeLapseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
