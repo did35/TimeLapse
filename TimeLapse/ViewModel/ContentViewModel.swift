6e1bf5864b4545e26800eb392bf83d2f406f3def
Didier <did35@yahoo.com>
2024-06-22 08:57:09 +0200

    [Feature] Setup.

---
+++ TimeLapse/ViewModel/ContentViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ContentViewModel.swift
+//  TimeLapse
+//
+//  Created by Didier Delhaisse on 22/06/2024.
+//
+
+import Foundation
+
+class ContentViewModel: ObservableObject {
+    @Published var someText = "Hello ViewModel!"
+}
--- TimeLapse/Views/ContentView.swift
+++ TimeLapse/Views/ContentView.swift
@@ -8,17 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    @StateObject private var vm = ContentViewModel()
+    
+    
+    // MARK: - Body
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(vm.someText)
         }
         .padding()
     }
 }
 
+// MARK: - Preview
 #Preview {
     ContentView()
 }
